bddb2518099534c8a786e85d039de565edc4e251
h.kumekawa <hkumekawa@gmail.com>
2017-05-11 23:48:24 +0900

    新しい関数を追加

--- handler.js
+++ handler.js
@@ -14,3 +14,12 @@ module.exports.hello = (event, context, callback) => {
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
 };
+
+module.exports.bye = (event, context, cb) => {
+
+  const response = {
+    message: 'good bye'
+  }
+
+  cb(null, response);
+}
